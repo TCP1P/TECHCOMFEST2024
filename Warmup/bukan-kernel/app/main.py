25ea0f232b16be6800cb0c36ad23ae28251d328d
apel <55012507+s3buahapel@users.noreply.github.com>
2023-12-23 11:39:39 +0700

    add chall bukan-kernel

---
+++ Warmup/bukan-kernel/app/main.py
@@ -0,0 +1,7 @@
+rahasia = 'ini sangat rahasia bjir, jadi kamu gabakal bisa tahu rahasianya wkwkwkkw'
+
+print "masukan password: "
+if rahasia == input(' '):
+    print 'kamu berhasil menebak rahasianya' 
+else:
+    print 'kamu gagal menebak rahasianya'
