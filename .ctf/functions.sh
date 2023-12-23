e0e620f38ddecb7423d9711f731a8e402bd84cfd
dimasma0305 <dimasmaulana0305@gmail.com>
2023-12-23 10:52:42 +0800

    add config

---
+++ .ctf/functions.sh
@@ -0,0 +1,21 @@
+export directories=$(find . -maxdepth 3 -type f \( -name "docker-compose.yaml" -o -name "docker-compose.yml" \) -exec dirname {} \; | sort -u)
+
+process_directories() {
+    IFS=$'\n'
+    for dir in $directories; do
+        if [[ "$dir" =~ \./\..* ]]; then
+            continue
+        fi
+
+        echo "Processing directory: $dir"
+
+        (
+            cd "$dir"
+            {
+                bash -c "$1"
+            }
+        )&
+    done
+
+    wait
+}
